0c09853367cff522fb01d336d29c28109ec048dd
Andrew Richard <acrichard07@gmail.com>
2025-02-25 16:15:51 -0800

    working on bonues questions

--- ar_sql_names.sql
+++ ar_sql_names.sql
@@ -14,19 +14,17 @@ SELECT SUM(num_registered) AS total_people
 FROM names;
 
 -- 3. Which name had the most appearances in a single year in the dataset?
-SELECT year, name, num_registered
+SELECT *
 FROM names
 ORDER BY num_registered DESC
 LIMIT 1;
 
 -- 4. What range of years are included?
-SELECT 
-	MIN(year) AS min_year,
-	MAX(year) AS max_year
+SELECT MIN(year) AS min_year,MAX(year) AS max_year
 FROM names;
 
 -- 5. What year has the largest number of registrations?
-SELECT year
+SELECT year, SUM(num_registered) AS most_registrations
 FROM names
 GROUP BY year
 ORDER BY SUM(num_registered) DESC
@@ -37,12 +35,12 @@ SELECT COUNT(DISTINCT(name)) AS n_distinct_names
 FROM names;
 
 -- 7. Are there more males or more females registered?
-SELECT COUNT(gender) AS n_male
+SELECT gender, SUM(num_registered)
 FROM names
-WHERE gender = 'M';
-SELECT COUNT(gender) AS n_female
-FROM names
-WHERE gender = 'F';
+GROUP BY gender;
+-- more males
+
+
 
 -- 8. What are the most popular male and female names overall (i.e., the most total registrations)?
 SELECT name, SUM(num_registered) AS most_popular_male
@@ -224,31 +222,138 @@ FROM names
 WHERE LOWER(name) = REVERSE(LOWER(name));
 
 -- 	3. Find all names that contain no vowels (for this question, we'll count a,e,i,o,u, and y as vowels). (Hint: you might find this page helpful: https://www.postgresql.org/docs/current/functions-matching.html)
-
+SELECT name
+FROM names
+WHERE name NOT SIMILAR TO '%(a*|e*|i*|o*|u*|y*)%';
 
 -- 	4. How many double-letter names show up in the dataset? Double-letter means the same letter repeated back-to-back, like Matthew or Aaron. Are there any triple-letter names?
+SELECT *
+FROM names
+WHERE LOWER(name) ~ '(.)\1';
 
-
+SELECT *
+FROM names
+WHERE LOWER(name) ~ '(.)\1\1'
 -- For the next few questions, you'll likely need to make use of subqueries. A subquery is a SQL query nested inside another query. You'll learn more about subqueries over the next few DataCamp assignments.
 
 
 -- 	5. On question 17 of the first part of the exercise, you found names that only appeared in the 1950s. Now, find all names that did not appear in the 1950s but were used both before and after the 1950s. We'll answer this question in two steps.
 
 -- 		a. First, write a query that returns all names that appeared during the 1950s.
+SELECT name
+FROM names
+WHERE year BETWEEN 1950 AND 1959
+GROUP BY name;
 
 -- 		b. Now, make use of this query along with the IN keyword in order the find all names that did not appear in the 1950s but which were used both before and after the 1950s. See the example "A subquery with the IN operator." on this page: https://www.dofactory.com/sql/subquery.
+SELECT name
+FROM names
+WHERE name NOT IN 
+	(SELECT name
+	FROM names
+	WHERE year BETWEEN 1950 AND 1959
+	GROUP BY name)
+GROUP BY name
+HAVING MIN(year) <1950
+	AND MAX(year) >1959;
 
 	
 -- 	6. In question 16, you found how many names appeared in only one year. Which year had the highest number of names that only appeared once?
+-- i need the years that have the most names that do not occur in any other year
+SELECT name
+FROM names
+GROUP BY name
+HAVING COUNT(year) = 1
+ORDER BY COUNT(year);
 
+SELECT year, COUNT(name) as n_names
+FROM names
+WHERE name IN
+	(
+	SELECT name
+	FROM names
+	GROUP BY name
+	HAVING COUNT(year) = 1
+	ORDER BY COUNT(year)
+	)
+GROUP BY year
+ORDER BY n_names DESC
+LIMIT 1;
 
 -- 	7. Which year had the most new names (names that hadn't appeared in any years before that year)? For this question, you might find it useful to write a subquery and then select from this subquery. See this page about using subqueries in the from clause: https://www.geeksforgeeks.org/sql-sub-queries-clause/
+SELECT DISTINCT name, MIN(year) AS year
+FROM names
+GROUP BY name
+ORDER BY year ASC;
 
+SELECT m.year, COUNT(name) AS new_names
+FROM (
+	SELECT DISTINCT name, MIN(year) AS year
+	FROM names
+	GROUP BY name
+	ORDER BY year ASC
+) AS m
+GROUP BY m.year 
+ORDER BY new_names DESC
+LIMIT 1;
 
 -- 	8. Is there more variety (more distinct names) for females or for males? Is this true for all years or are their any years where this is reversed? Hint: you may need to make use of multiple subqueries and JOIN them in order to answer this question.
+SELECT gender,COUNT(DISTINCT name) AS distinct_names
+FROM names
+GROUP BY gender;
+-- overall more distinct female names than male names
+SELECT *
+FROM (
+	SELECT year, m.distinct_male_names, f.distinct_female_names
+	FROM (
+		SELECT year, COUNT(DISTINCT name) AS distinct_male_names
+		FROM names
+		WHERE gender = 'M'
+		GROUP BY year
+	) AS m
+	INNER JOIN (
+		SELECT year, COUNT(DISTINCT name) AS distinct_female_names
+		FROM names
+		WHERE gender = 'F'
+		GROUP BY year
+	) AS f
+	USING(year)
+)
+WHERE distinct_male_names > distinct_female_names;
+-- only three years where there were more distinct male names than femal names
 
 
 -- 	9. Which names are closest to being evenly split between male and female usage? For this question, consider only names that have been used at least 10000 times in total. 
+-- query to get names used 10000 times at least
+SELECT name, SUM(num_registered)
+FROM names
+GROUP BY name
+HAVING SUM(num_registered)> 10000
+ORDER BY SUM(num_registered);
+
+
+-- output should show name, n_female, n_male, n_diff
+-- order by n_diff, smallest are closest to even split
+-- join on name
+
+SELECT name, m.n_male, f.n_female, ABS(m.n_male - f.n_female) AS n_diff
+FROM(
+	SELECT name, SUM(num_registered) AS n_male
+	FROM names
+	WHERE gender = 'M'
+	GROUP BY name
+	HAVING SUM(num_registered) > 10000
+) AS m
+INNER JOIN (
+	SELECT name, SUM(num_registered) AS n_female
+	FROM names
+	WHERE gender = 'F'
+	GROUP BY name
+	HAVING SUM(num_registered) > 10000
+) AS f
+USING(name)
+ORDER BY n_diff
+LIMIT 5;
 
 
 -- For the last questions, you might find window functions useful (see https://www.postgresql.org/docs/current/sql-expressions.html#SYNTAX-WINDOW-FUNCTIONS and https://www.postgresql.org/docs/current/functions-window.html for a list of window function available in PostgreSQL). A window function is like an aggregate function in that it can be applied across a group, but a window function does not collapse each group down to a single summary statistic. The groupings for a window function are specified using the PARTITION BY keyword (and can include an ORDER BY when it is needed). The PARTITION BY and ORDER BY associated with a window function are CONTAINED in an OVER clause.
